d4c9289195e8cf61e0162dd6deb890991236e312
emmanuelmaldonado <andymaldonadolop60@gmail.com>
2026-02-22 18:10:44 -0600

    se solicitaron las entradas principales para la clasificación

--- Ejercicio 4/Program.cs
+++ Ejercicio 4/Program.cs
@@ -1,2 +1,14 @@
 ﻿// Clasificación de incidente de seguridad informática
 
+Console.WriteLine("Clasificación de incidente");
+Console.WriteLine("Tipo de incidente: 1 Malware, 2 Phishing, 3 Acceso no autorizado, 4 Fuga de información");
+int tipoincidente = int.Parse(Console.ReadLine());
+Console.WriteLine("Activo afectado: 1 PC, 2 Servidor, 3 Base de datos, 4 Red");
+int activoafectado = int.Parse(Console.ReadLine());
+Console.WriteLine("Datos comprometidos: 1 Ninguno, 2 Internos, 3 Personales, 4 Financieros");
+int datoscomprom = int.Parse(Console.ReadLine());
+Console.WriteLine("Persistencia detectada (s/n)");
+string persistencia = Console.ReadLine();
+Console.WriteLine("Número de usuarios afectados");
+int usuariosafect = int.Parse(Console.ReadLine());
+
